deed64b65427ba93f71c68229a2cede81b4cb32d
Immortal <shukladev2027@gmail.com>
2025-11-05 12:14:50 +0530

    Js and data structure practice

---
+++ Web-Security/2_practice.js
@@ -0,0 +1,32 @@
+// // Object Practice
+// const customer = {
+//     // key: value
+//     name: "John",
+//     age: 30,
+//     city: "New York"
+// };
+
+// customer.age = 20;
+// customer.name = "Immortal";
+
+// // Below I've shown two ways to access object properties, but more prefferred is object.key method.
+// console.log(customer.name);
+// console.log(customer["name"]);
+
+// console.log(customer.age);
+// console.log(customer.city);
+
+
+// // Practice Problems
+
+// const Product = {
+//     title: "Parker Jotter Standard CT Ball Pen, (Black)",
+//     rating: 4,
+//     isDeal: true,
+//     Price: 270
+// };
+
+// console.log(Product.title);
+// console.log(Product.rating);
+// console.log(Product.isDeal);
+// console.log(Product.Price);
---
+++ Web-Security/3_practice.js
@@ -0,0 +1,20 @@
+const profile = {
+    username: "shradhakhapra",
+    isFollow: true,
+    post: 195,
+    followers: 569 + "K",
+    following: 4,
+    fullName: "Shradha Khapra",
+    profession: "Entrepreneur",
+    about: "Apna College | Ex-Microsoft, DRDO \nTo educate someone is the highest privilege"
+};
+
+console.log(profile.username);
+console.log(profile.isFollow);
+console.log(profile.post);
+console.log(profile.followers);
+console.log(profile.following);
+console.log(profile.fullName);
+console.log(profile.profession);
+console.log(profile.about);
+console.log(profile.description);
