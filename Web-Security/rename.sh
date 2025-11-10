135873b8ae3b26a12d8365c876315c45f7db4255
Immortal <shukladev2027@gmail.com>
2025-11-10 11:55:07 +0530

    added some more practice files

---
+++ Web-Security/rename.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Below is a for loop to rename file from 1 to 9 to 01 to 09.
+# for i in {1..9}; do
+#   mv "${i}_practice.js" "0${i}_practice.js"
+# done
+
+# Below is a for loop to create files from 30 to 40, edit it as needed.
+# for i in {30..40}; do
+#   touch ${i}_practice.js
+# done
